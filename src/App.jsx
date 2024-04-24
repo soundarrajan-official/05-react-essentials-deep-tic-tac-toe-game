0411b4346f61f5289303e62b785d2eb4817095e4
soundarrajan-official <soundarrajan.official@gmail.com>
2024-04-24 12:03:38 +0530

    Add list of players JSX code in App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-
 function App() {
-  
 
-  return (
-    <p>Coming Soon...</p>
-  )
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <li>
+                        <span className="player">
+                          <span className="player-name">Player 1</span>
+                          <span className="player-symbol">X</span>
+                        </span>
+                        <button>Edit</button>
+                    </li>
+                    <li>
+                        <span className="player">
+                          <span className="player-name">Player 2</span>
+                          <span className="player-symbol">O</span>
+                        </span>
+                        <button>Edit</button>
+                    </li>
+                </ol>
+                GAME BOARD
+            </div>
+        </main>
+    )
 }
 
 export default App
